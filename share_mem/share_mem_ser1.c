8f6b90efa9300d17c3a584e9293478768461cbb4
luhua187 <luhua187@126.com>
2019-12-05 17:01:11 +0800

    share memory

---
+++ share_mem/share_mem_ser1.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <string.h>
+#include <fcntl.h>
+#include <stdlib.h>
+
+#define PRI_SHM_ID 1024
+#define PATH_SHM "home/lu/"
+
+int main()
+{
+	key_t key_v = 0;
+	char *p = NULL;
+	int nshmid = 0;
+
+	key_v = ftok(PATH_SHM, PRI_SHM_ID);
+
+	nshmid = shmget(key_v, 1024, IPC_CREAT|0666);
+	if(nshmid == -1)
+	{
+		printf("create share memory failed\n");
+		return 0;
+	}
+
+	p =  (char *)shmat(nshmid, 0, 0);
+	if(p == NULL)
+	{
+		printf("get share memory error!\n");
+		return 0;
+	}
+
+	printf("@_@ [%s]\n", p);
+
+	shmdt(p);
+
+	return 0;
+}
